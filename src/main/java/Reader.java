87aeb544b8b1e7e70efca0b54bb386bd9151e3e9
kudai <kuanysh1994k@mail.ru>
2020-04-28 17:49:52 +0300

    рефактор

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -5,6 +5,8 @@ import lombok.SneakyThrows;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,39 +18,43 @@ import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
 @NoArgsConstructor
 class Reader implements Runnable {
 
-	private String fileName;
+    private String fileName;
 
-	@SneakyThrows
-	public void run() {
-		XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(fileName, new FileInputStream(fileName));
-		List<Double> sides = new ArrayList<>();
-		StringBuilder color = new StringBuilder();
-		boolean readColor = false;
-		int number = 0;
+    @SneakyThrows
+    public void run() {
+        try (InputStream inputStream = new FileInputStream(fileName)) {
+            XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(fileName, inputStream);
+            List<Double> sides = new ArrayList<>();
+            StringBuilder color = new StringBuilder();
+            boolean readColor = false;
+            int number = 0;
 
-		while (reader.hasNext()) {
-			reader.next();
-			if (reader.isStartElement()) {
-				if (equalsIgnoreCase(reader.getLocalName(), COLOR)) {
-					readColor = true;
-					color.setLength(0);
-				} else {
-					readColor = false;
-				}
-			} else if (reader.isEndElement()) {
-				if (equalsAnyIgnoreCase(reader.getLocalName(), Calculate.TRIANGLE, Calculate.RECTANGLE, Calculate.SQUARE, Calculate.CIRCLE)) {
-					Calculate.calculate(number, reader.getLocalName(), color.toString(), sides);
-					number++;
-				}
-			} else if (reader.hasText() && reader.getText().trim().length() > 0) {
-				if (readColor) {
-					color.append(reader.getText());
-				} else {
-					sides.add(Double.valueOf(reader.getText()));
-				}
-			}
-		}
-	}
+            while (reader.hasNext()) {
+                reader.next();
+                if (reader.isStartElement()) {
+                    if (equalsIgnoreCase(reader.getLocalName(), COLOR)) {
+                        readColor = true;
+                        color.setLength(0);
+                    } else {
+                        readColor = false;
+                    }
+                } else if (reader.isEndElement()) {
+                    if (equalsAnyIgnoreCase(reader.getLocalName(), Calculate.TRIANGLE, Calculate.RECTANGLE, Calculate.SQUARE, Calculate.CIRCLE)) {
+                        Calculate.calculate(number, reader.getLocalName(), color.toString(), sides);
+                        number++;
+                    }
+                } else if (reader.hasText() && reader.getText().trim().length() > 0) {
+                    if (readColor) {
+                        color.append(reader.getText());
+                    } else {
+                        sides.add(Double.valueOf(reader.getText()));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не найден файл " + fileName);
+        }
+    }
 
-	private static final String COLOR = "color";
+    private static final String COLOR = "color";
 }
